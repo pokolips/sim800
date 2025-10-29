cb246188834c8886a5ca978aa4ab5223b4846724
pokolips <pokolips@gmail.com>
2025-10-30 01:28:48 +0300

    Проверка модуля

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
+/* http://codius.ru/articles/GSM_модуль_SIM800L_часть_1*/
+
+
 #include <Arduino.h>
 
-// put function declarations here:
+#include <SoftwareSerial.h>
+SoftwareSerial SIM800(8, 9);        // 8 - RX Arduino (TX SIM800L), 9 - TX Arduino (RX SIM800L)
+void setup() {
+  Serial.begin(9600);               // Скорость обмена данными с компьютером
+  Serial.println("Start!");
+  SIM800.begin(9600);               // Скорость обмена данными с модемом
+  SIM800.println("AT");
+}
+
+void loop() {
+  if (SIM800.available())           // Ожидаем прихода данных (ответа) от модема...
+    Serial.write(SIM800.read());    // ...и выводим их в Serial
+  if (Serial.available())           // Ожидаем команды по Serial...
+    SIM800.write(Serial.read());    // ...и отправляем полученную команду модему
+}
+/* put function declarations here:
 int myFunction(int, int);
 
 void setup() {
@@ -15,4 +33,4 @@ void loop() {
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
-}
+}*/
